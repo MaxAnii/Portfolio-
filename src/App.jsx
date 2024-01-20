f00ce2b32b549dbd03e896fcc40cc04058699956
MaxAnii <ansarbaba2000@gmail.com>
2024-01-20 23:46:38 +0530

    show loader for 1.5 seconds.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { React, useEffect, useState } from "react";
 import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import Loader from "./components/Loader";
 function App() {
+  const [loader, setLoader] = useState(true);
+
   useEffect(() => {
     AOS.init();
+    setTimeout(() => setLoader(false), 1500);
   }, []);
   return (
     <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      {/* <Route path="/home" element={}></Route> */}
+      <Route
+        path="/"
+        element={loader ? <Loader></Loader> : <Home></Home>}
+      ></Route>
     </Routes>
   );
 }
