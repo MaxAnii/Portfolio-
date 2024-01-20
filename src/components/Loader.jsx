651982a08b93fa8b88e77bc60257a25995860646
MaxAnii <ansarbaba2000@gmail.com>
2024-01-20 23:47:11 +0530

    created a loader component.

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader text-3xl"></div>
+    </div>
+  );
+};
+
+export default Loader;
