d801c50f3bda6979d351db552c7b90cd6f70c6cb
MaxAnii <ansarbaba2000@gmail.com>
2024-01-25 21:52:56 +0530

    Arranged the project order vise.

--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -2,17 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Image } from "@nextui-org/react";
 const ProjectContainer = (props) => {
-  const imageClassName = `md:pt-[10vh] lg:pt-0 lg:pl-5 px-3 pt-5 lg:order-${props.imageOrder}`;
-  const descriptionClassName = `md:pl-10 pl-3 pt-5 lg:w-[40vw] lg:pt-0 lg:order-${props.descriptionOrder}`;
+  let imageOrder = 1;
+  let descriptionOrder = 2;
+  if (props.order % 2 === 0) {
+    imageOrder = 2;
+    descriptionOrder = 1;
+  }
+  console.log(
+    `lg:order-${imageOrder} image`,
+    `lg:order-${descriptionOrder} des`
+  );
   return (
     <>
       <div className="md:pl-[10vw] md:pr-[10vw] lg:pt-[9vh] md:p-5 lg:overflow-hidden lg:flex">
-        <div className={imageClassName}>
+        <div
+          className={`md:pt-[10vh] lg:pt-0 lg:pl-5 px-3 pt-5 lg:order-${imageOrder}`}
+        >
           <div className="" data-aos="zoom-in">
             <Image isBlurred src={props.image} className="lg:w-[40vw]"></Image>
           </div>
         </div>
-        <div className={descriptionClassName}>
+        <div
+          className={`md:pl-10 pl-3 pt-5 lg:w-[40vw] lg:pt-0 lg:order-${descriptionOrder}`}
+        >
           <h3 className="text-3xl text-center md:text-left md:p-0 w-auto">
             {props.projectName}
           </h3>
