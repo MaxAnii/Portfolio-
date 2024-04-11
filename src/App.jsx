7ce5aadefe374364cd0f381fe3a889073a9d7511
MaxAnii <ansarbaba2000@gmail.com>
2024-04-11 22:44:01 +0530

    set the time of loader to 3 sec.

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Loader from "./components/Loader";
 function App() {
-  const [loader, setLoader] = useState(true);
+	const [loader, setLoader] = useState(true);
 
-  useEffect(() => {
-    AOS.init();
-    setTimeout(() => setLoader(false), 2000);
-  }, []);
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={loader ? <Loader></Loader> : <Home></Home>}
-      ></Route>
-    </Routes>
-  );
+	useEffect(() => {
+		AOS.init();
+		setTimeout(() => setLoader(false), 3000);
+	}, []);
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={loader ? <Loader></Loader> : <Home></Home>}
+			></Route>
+		</Routes>
+	);
 }
 export default App;
